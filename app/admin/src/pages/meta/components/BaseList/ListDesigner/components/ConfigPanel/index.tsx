33904a6a1d2dbbad098e00924d5a6a25d5998692
xiangyunfei <xiangyunfei@meituan.com>
2026-02-13 21:34:37 +0800

    fix: 列表调整

--- app/admin/src/pages/meta/components/BaseList/ListDesigner/components/ConfigPanel/index.tsx
+++ app/admin/src/pages/meta/components/BaseList/ListDesigner/components/ConfigPanel/index.tsx
@@ -20,16 +20,16 @@ const ConfigPanel: React.ForwardRefRenderFunction<ListConfigRef, ListConfigProps
 
   return (
     <ProForm form={form} submitter={false} initialValues={config} layout="vertical">
+      <ProForm.Item name="helpSettings" layout="horizontal" tooltip="配置表单帮助" label="帮助设置">
+        <HelpSettingsButton />
+      </ProForm.Item>
+
       <ProFormSwitch name="frozenColumn" label="冻结操作列" />
       <ProFormSwitch name="showActions" label="显示操作列" />
       <ProFormSwitch name="showCheckbox" label="显示勾选框" />
       <ProFormSwitch name="showIndex" label="显示序号" />
       <ProFormDigit name="frozenColumnNum" label="左侧冻结列数量" min={0} max={10} />
       <ProFormDigit name="pageSize" label="每页显示数据" min={1} max={1000} />
-
-      <ProForm.Item name="helpSettings" layout="horizontal" tooltip="配置表单帮助" label="帮助设置">
-        <HelpSettingsButton />
-      </ProForm.Item>
     </ProForm>
   );
 };
